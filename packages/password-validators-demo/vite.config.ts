1f31de35a467a0fa889757116071c11796bf7554
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2025-05-04 20:30:01 -0300

    more fixes in monorepo

--- packages/password-validators-demo/vite.config.ts
+++ packages/password-validators-demo/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/password-validators/',
+  resolve: {
+    alias: {
+      '@slifszyc/password-validators': path.resolve(
+        __dirname,
+        '../password-validators/src'
+      ),
+    },
+  },
 })
